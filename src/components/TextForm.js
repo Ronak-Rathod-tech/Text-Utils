9618735ab393df505d2ee6bdbb2c8e9800b85c56
Ronak-Rathod <rathodronak444@gmail.com>
2024-11-27 22:43:01 +0530

    Complete code of text-utils application

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,46 +1,50 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 
 export default function TextForm(props) {
-  const handleUpClick = () =>{
+  const handleUpClick = () => {
     console.log("Uppercash was clicked" + text);
     let newText = text.toUpperCase();
     setText(newText);
 
   }
 
-  const handleLoClick = () =>{
+  const handleLoClick = () => {
     console.log("Lower was clicked" + text);
     let newText = text.toLowerCase();
     setText(newText);
 
   }
 
-  const handleOnChange = (event) =>{
+  const handleOnChange = (event) => {
     console.log("On change");
     setText(event.target.value);
   }
 
-  const [text, setText] = useState ('Enter text here');
+  const [text, setText] = useState('Enter text here');
   return (
     <>
-    <div className='container' style={{color: props.mode==='dark'?'white':'black'}}>
-            <h1>{props.heading}</h1>
-            <div className="mb-3">
-            <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'grey':'white', color: props.mode==='dark'?'white':'black' }} id="myBox" rows="8"></textarea>
-            </div>
-            <button className='btn btn-primary mx-2 my-1' onClick={handleUpClick}>Convert To Uppercase</button>
-            <button className='btn btn-primary mx-2 my-1' onClick={handleLoClick}>Convert To Lowercase</button>
-    </div>
+      <div className='container' style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea className="form-control" value={text} onChange={handleOnChange} style={{ backgroundColor: props.mode === 'dark' ? 'grey' : 'white', color: props.mode === 'dark' ? 'white' : 'black' }} id="myBox" rows="8"></textarea>
+        </div>
+        <button className='btn btn-primary mx-2 my-1' onClick={handleUpClick}>Convert To Uppercase</button>
+        <button className='btn btn-primary mx-2 my-1' onClick={handleLoClick}>Convert To Lowercase</button>
+      </div>
 
-    <div className='container my-3' style={{color: props.mode==='dark'?'white':'black'}}>
-      <h1>Your text summary</h1>
-      <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
-      <p>{0.008 * text.split(" ").length} Minuts read</p>
-      <h2>Preview</h2>
-      <p>{text.length>0?text:'Enter something to preview it here'}</p>
 
-    </div>
+
+
+
+      <div className='container my-3' style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
+        <h1>Your text summary</h1>
+        <p>{text.split(" ").filter((element) => { return element.length !== 0 }).length} words and {text.length} characters</p>
+        <p>{0.008 * text.split(" ").length} Minuts read</p>
+        <h2>Preview</h2>
+        <p>{text.length > 0 ? text : 'Enter something to preview it here'}</p>
+
+      </div>
     </>
   )
 }
